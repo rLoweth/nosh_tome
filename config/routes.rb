ded05a489dad0e9e061b26f9cf887b36e60b0f0f
rLoweth <62730032+rLoweth@users.noreply.github.com>
2021-05-31 03:56:25 -0400

    finished controller for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      post 'recipes/create'
+      get '/show/:id', to: 'recipes#show'
+      dlede '/recipes/:destroy', to: 'recipes#delete'
+    end
+  end
   root 'homepage#index'
+  get '/*path' => 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
